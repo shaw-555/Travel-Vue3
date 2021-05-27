1f5aa6e4ff1827b43b35875d9b60d720f8043fe2
shaw-555 <shaw18664590570@163.com>
2021-05-27 21:29:25 +0800

    swiper组件

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,18 @@
+<template >
+  <div>
+    it is the swiper
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper.stylus'
+export default {
+  name: 'HomeSwiper'
+}
+</script>
+
+<style lang="stylus" scoped>
+  div
+    color red
+</style>
