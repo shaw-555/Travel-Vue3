fe5dd800d89408739effeb2d42547da73dc3b4e1
shaw-555 <shaw18664590570@163.com>
2021-06-02 21:59:37 +0800

    list页面初步

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,6 +1,44 @@
 <template>
-  <div>
-    it is the list
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="buuton-list">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+            <div class="button-wrapper">
+              <div class="button">北京</div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button">上海</div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button">天津</div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button">南京</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">xxx</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +47,43 @@ export default {
   name: 'CityList'
 }
 </script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+    &:before 
+      border-color: #ccc
+  .list
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+  .title 
+    line-height .44rem
+    background-color #eee
+    padding-left .2rem
+  .button-list
+    overflow hidden
+    padding .1rem .6rem .1rem .1rem
+    .button-wrapper
+      float: left
+      width: 33.3%
+      .button
+        margin: .1rem
+        padding: .1rem 0
+        text-align: center
+        border: .02rem solid #ccc
+        border-radius: .3rem
+  .item-list
+    .item 
+      line-height .76rem
+      color #666
+      padding-left .2rem
+      text-align center
+</style>
